896c0a38ace415e084bf6347689e83b3756bb5bf
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2018-01-18 13:22:05 -0200

    Foram criadas as models, controllers e migrations das tabelas fornecedores e telefones_fornecedores.

---
+++ app/Fornecedor/Telefone.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Fornecedor;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Telefone extends Model
+{
+    //
+}
---
+++ database/migrations/fornecedores/2018_01_18_113026_telefone_dos_fornecedores.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TelefoneDosFornecedores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telefones_fornecedores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('fornecedor_id')->unsigned('id');
+            $table->unsignedInteger('numero_telefone');
+            $table->timestamps();
+        });
+
+        //This code attributes "fornecedor_id" as a foreign key and sign it.
+        Schema::table('telefones_fornecedores', function (Blueprint $table) {
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telefones_fornecedores');
+    }
+}
